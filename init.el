ef86cdda672c861aa6702910540c1712dbcf3222
Oleg <xvoleg@gmail.com>
2024-12-18 20:57:51 +0100

    Updated counsel M-x key: "C-x :" +ivy immediate-dome in Min.Buffers.
    +ivy minibuffer map added -immediate-done "C-i" for cases like with
    ~org-cite-insert~ which is just getting stuck there.

--- init.el
+++ init.el
@@ -122,10 +122,11 @@
   :diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
+         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
+				 ("C-i" . ivy-immediate-done)
          :map ivy-switch-buffer-map
          ("C-k" . ivy-previous-line)
          ("C-l" . ivy-done)
@@ -137,7 +138,7 @@
   (ivy-mode 1))
 
 (use-package counsel
-  :bind (("M-x" . counsel-M-x)
+  :bind (("C-x :" . counsel-M-x)
 				 ("C-x b" . counsel-ibuffer)
 				 ("C-x C-b" . counsel-ibuffer)
 	 ("C-x C-f" . counsel-find-file)
